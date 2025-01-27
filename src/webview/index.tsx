a26625aa26688148e7959d1c253c1576ed6f6ba6
kthurman59 <kthurman59@gmail.com>
2025-01-27 05:21:22 -0600

    index config updated

---
+++ src/webview/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import './styles.css';
+
+// Establish VS code webview API connection
+declare global {
+    interface Window {
+        acquireVsCodeApi(): any,
+    }
+}
+
+const vscode = window.acquireVsCodeApi();
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <React.StrictMode>
+        <App vscode={vscode} />
+    <React.StrictMode>
+);
+
